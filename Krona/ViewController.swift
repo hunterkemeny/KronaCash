b98cca64a291e6b749d331504bc286dfdd383536
Hunter Kemeny <alekkemeny@gmail.com>
2019-07-15 21:05:02 -0700

    Initial Commit

---
+++ Krona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Krona
+//
+//  Created by Alek  Kemeny on 7/15/19.
+//  Copyright © 2019 Krona Technologies Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
