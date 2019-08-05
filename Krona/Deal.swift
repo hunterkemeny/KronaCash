d3933a633072d8be2b6bd2440031e83970afa463
Bilal Kapadya <bilalnoork@hotmail.com>
2019-08-04 17:53:06 -0700

    Create god class.

---
+++ Krona/Deal.swift
@@ -0,0 +1,21 @@
+//
+//  Deal.swift
+//  Krona
+//
+//  Created by Bilal Kapadya on 8/4/19.
+//  Copyright © 2019 Krona Technologies Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class Deal
+{
+    
+    var name: String?
+    var description: String?
+    
+    init()
+    {
+        
+    }
+}
